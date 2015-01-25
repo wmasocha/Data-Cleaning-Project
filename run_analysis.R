fcbb464cbbacd3267502023e4a6d836aca92cad0
wmasocha <wmasocha@yahoo.com>
2015-01-25 14:51:37 +0300

    Script for producing tidy data

---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+library("plyr")
+library("data.table")
+library("reshape2")
+
+## Script should do the following:
+## 1. Merge the training and the test sets to create one data set.
+## 2. Extract only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately label the data set with descriptive activity names.
+## 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+# Read activity labels and data column names
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+
+# select only the means and standard deviations for each measurement.
+extract_features <- grepl("mean|std", features)
+
+# Read X_test, y_test,subject_test, X_train, y_train and subject_train data
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+# Assign names
+names(X_test) = features
+names(X_train) = features
+
+# select only the means and standard deviations for each measurement.
+X_test = X_test[,extract_features]
+X_train = X_train[,extract_features]
+
+# Load activity labels and label the data set with descriptive activity names.
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+
+#  Merge the training and the test sets to create one data set
+data = rbind(cbind(as.data.table(subject_test), y_test, X_test), cbind(as.data.table(subject_train), y_train, X_train))
+
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+
+# Apply mean function to dataset using dcast function
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+## Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_data_mean <- ddply(melt(tidy_data, id.vars=c("subject", "Activity_Label")), .(subject, Activity_Label), summarise, MeanSamples=mean(value))
+
+write.table(tidy_data, file = "./tidy_data.txt")
+write.table(tidy_data_mean, file = "./tidy_data_mean.txt")
